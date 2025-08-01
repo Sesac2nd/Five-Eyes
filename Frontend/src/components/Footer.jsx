274ffbf499caff381879356ed4d7e7008bcb67a5
jiwoo <sjw01827@gmail.com>
2025-08-01 17:23:12 +0900

    feat: 라우터 환경 세팅

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>© 2025 역사로(HistPath) - 국사편찬위원회 조선왕조실록 기반</div>
+    </footer>
+  );
+}
+
+export default Footer;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // 여기서 BrowserRouter를 가져옵니다
+import './index.css';
+import App from './pages/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
